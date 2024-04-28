41a561deff3275dcf34ef60ab3ff408159b66df5
Simone Picciani <zanza00@gmail.com>
2024-04-29 00:17:07 +0200

    log defects

--- cli.ts
+++ cli.ts
@@ -1,9 +1,20 @@
-import { Effect, Layer, Ref } from "effect";
+import { Cause, Console, Effect, Layer, Ref } from "effect";
 import { program } from "./src/mod.ts";
 import { CounterStateLive } from "./src/CounterState.ts";
 
 // https://deno.land/manual/tools/script_installer
 if (import.meta.main) {
 	const runnable = program(Deno.args).pipe(Effect.provide(CounterStateLive));
-	Effect.runFork(runnable);
+	Effect.runFork(
+		runnable.pipe(
+			Effect.catchAllDefect((defect) => {
+				if (Cause.isRuntimeException(defect)) {
+					return Console.log(
+						`RuntimeException defect caught: ${defect.message}`,
+					);
+				}
+				return Console.log("Unknown defect caught.");
+			}),
+		),
+	);
 }
